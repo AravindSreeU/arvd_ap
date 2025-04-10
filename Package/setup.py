887cab75312c7dfc5c5733cd644099777954f6f6
RVD_THE_THINKER <95367109+AravindSreeU@users.noreply.github.com>
2025-04-10 15:55:55 +0530

    Update setup.py

--- Package/setup.py
+++ Package/setup.py
@@ -1,18 +1,22 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='arvd_ap',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        # List your dependencies here
+        # Add dependencies if needed
     ],
     author='ARAVIND SREE U',
     author_email='uaravindsree@gmail.com',
     description='My description will help you to pass time and understand my ability of writing codes',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/AravindSreeU/rvd_ap',
+    url='https://github.com/AravindSreeU/arvd_ap',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
